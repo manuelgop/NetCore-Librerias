beb9922b46a02dd5b21f738e10a7e8130cece1a4
Manuel Gopar <manuelgop@gmail.com>
2018-03-25 22:43:09 -0600

    Agregando Lib

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using stringModifier;
+namespace libraryRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(ChangeMyString.makeAllUppercase("Hello World"));
+            Console.WriteLine(ChangeMyString.makeReverse("Hello World"));
+
+
+        }
+    }
+}
